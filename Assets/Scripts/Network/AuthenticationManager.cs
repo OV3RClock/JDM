688812dc8a21b41780f8f4cfb3e413d3802b4fb5
OVERClock <40008329+OV3RClock@users.noreply.github.com>
2022-10-08 21:06:13 +0200

    Updated Authentication pkg + few fixes

--- Assets/Scripts/Network/AuthenticationManager.cs
+++ Assets/Scripts/Network/AuthenticationManager.cs
@@ -1,20 +1,50 @@
+using System.Threading.Tasks;
 using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class AuthenticationManager : MonoBehaviour 
 {
+    [SerializeField] private DiscordDataSO _discordData;
+    
     private string playerId = "Not signed in";
+
+
+    private string idProviderName = "";
+    private string idToken = "";
+
 
     public async void LoginButton()
     {
         Debug.Log("Signing On...");
 
-        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        await SignInWithOpenIdConnectAsync(idProviderName, idToken);
 
         playerId = AuthenticationService.Instance.PlayerId;
 
         Debug.Log("AuthenticationManager : playerId : " + playerId);
+    }
+
+    private async Task SignInWithOpenIdConnectAsync(string idProviderName, string idToken)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithOpenIdConnectAsync(idProviderName, idToken);
+            Debug.Log("SignIn is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
     }
 
 }
